2e772f0f29f86c9a445bad599c002605c1d90929
Chris Carlton <ccarlton@chapman.edu>
2019-12-05 12:34:37 -0800

    Adds cars.sh

---
+++ cars.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#cars.sh
+#Chris Carlton
+
+x=1
+while [ "$x" -eq "1" ]
+do
+	echo "Type the number 1 to enter a new car"
+	echo "Type the number 2 to display the list of cars"
+	echo "Type the number 3 to quit and exit the program"
+	read var
+
+	case "$var" in
+		"1") echo "What year is the car"
+		read year 
+		echo "What is the make of the car"
+		read make
+		echo "What is the model of the car"
+		read model
+		result="$year:$make:$model"
+		echo "$result" >> My_old_cars;;
+
+		"2")sort My_old_cars
+ 		echo "$My_old_cars";;
+
+		"3") echo "Goodbye!"
+		x=0;;
+		
+		*)
+	esac
+done
